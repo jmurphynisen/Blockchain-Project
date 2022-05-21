dab6f7dde8224c5f9610f516736c3029712bba8a
John Murphy <73606870+jmurphynisen@users.noreply.github.com>
2022-05-21 18:11:06 -0400

    added resolve chain

--- Blockchain.py
+++ Blockchain.py
@@ -7,6 +7,10 @@ from flask import Flask, jsonify, request
 import random
 import hmac
 from urllib.parse import urlparse
+import requests
+import time
+
+from sklearn import neighbors
 
 class Blockchain(object):
     """
@@ -26,19 +30,21 @@ class Blockchain(object):
         Create genesis block
         Returns: new block 
         """
-        return self.createBlock(previousHash=1, proof=10000)
 
-    def createBlock(self, proof, previousHash):
+        return self.createBlock(previousHash=1, proof=10000, cid='0000')
+
+    def createBlock(self, proof, previousHash, cid):
         """
         Pushes new block to candidate queue, then passes it to be validated and mined by chosen third party node (n3)
         Returns: created block
         """
         newBlock = {
             'index': len(self.masterChain) + 1,
-            'timestamp': time(),
+            'timestamp': time.asctime(time.localtime()),
             'revision': self.revisionQueue,
-            'cID': self.revisionQueue['CID'],
+            'CID': cid,
             'proof': proof,
+            'location': self.getLocation(cid),
             'previousHash': previousHash or self.hash(self.masterChain[-1]),
         }
 
@@ -48,7 +54,7 @@ class Blockchain(object):
 
         if len(self.revisionQueue) != 0: self.revisionQueue.pop(-1)
 
-        print("New block added at " + time())
+        print("New block added at " + str(time.asctime(time.localtime())))
 
         return newBlock
 
@@ -66,7 +72,7 @@ class Blockchain(object):
 
         return keySignature
 
-    def getLocation(cid):
+    def getLocation(self, cid):
         """
         Use file system api to retrive location by cid or whatever file is stored as 
         """
@@ -77,17 +83,28 @@ class Blockchain(object):
         Pushes new revision with editor (n2), author (n1), the location of the file, new raw data, CID, id of revision
         """
         revisionID = random.getrandbits(128)
-        self.revisionQueue.append({
-            'author': author,
-            'editor': editor or None,
-            'editorKey': self.generateKey() or None,
-            'authorKey': self.generateKey(),
-            'CID': cid,
-            'time': time(),
-            'location': self.getLocation(cid),
-            'revisionID': revisionID,
-            'rawData': hashlib.sha256(rawData)
-        })
+        if editor == None:
+            self.revisionQueue.append({
+                'author': author,
+                'editor': editor,
+                'editorKey': -1,
+                'authorKey': self.generateKey(),
+                'CID': cid,
+                'time': time.asctime(time.localtime()),
+                'revisionID': revisionID,
+                'rawData': hashlib.sha256(rawData)
+            })
+        else:
+            self.revisionQueue.append({
+                'author': author,
+                'editor': editor,
+                'editorKey': self.generateKey(),
+                'authorKey': self.generateKey(),
+                'CID': cid,
+                'time': time.asctime(time.localtime()),
+                'revisionID': revisionID,
+                'rawData': hashlib.sha256(rawData)
+            })
 
         return self.lastBlock['index'] + 1
 
@@ -113,7 +130,7 @@ class Blockchain(object):
         Collaborative proof of work, where both nodes n1 and n2 give some statrting proof (just some integer), and continue until proof has 4 leading 0s
         Returns: n1 and n2's proof as a tuple
         """
-        while self.validateProof(n1proof, n2proof) is False:
+        while self.validateProof(n1proof, n2proof) == False:
             n1proof += 1
             n2proof += 1
 
@@ -122,10 +139,10 @@ class Blockchain(object):
     def validateProof(n1proof, n2proof):
         """
         Creates hash of n1 and n2's proof
-        Returns: if 2 leading digits are 0 then True, False otherwise
+        Returns: if 4 leading digits are 0 then True, False otherwise
         """
         hash = hashlib.sha256(str(n1proof + n2proof).encode()).hexdigest()
-        return hash[:4] == '00'
+        return hash[:4] == '0000'
     
     def validateChain(self):
         """
@@ -142,4 +159,39 @@ class Blockchain(object):
         Register new node on network with given address
         """
         url = urlparse(address)
-        self.nodes.append(url.netloc)
+        if url.netloc:
+            self.nodes.append(url.netloc)
+            return True
+        elif url.path:
+            self.nodes.append(url.path)
+            return True
+        else:
+            print("Could not add URL")
+            return False
+  
+    def resolveChain(self):
+        """
+        If at least two chains from nodes on network conflict with each other, get the most up-to-date chain (longest)
+        and replace each node's chain with that one.
+        """
+        neighbors = self.nodes
+        longestChain = None
+
+        currentChainLength = len(self.masterChain)
+
+        neighborsTemp = neighbors
+        i = 0
+        while(neighborsTemp):
+            response = requests.get(f'http://{neighborsTemp[i]}/globalchain')
+
+            if response.status_code == 200 and response.json()['length'] > currentChainLength:
+                longestChain = response.json()['chain']
+            
+            i += 1
+            neighborsTemp.pop(-1)
+
+        if longestChain != None:
+            self.masterChain = longestChain
+            return True
+        else: 
+            return False
